df46322301be23a5d3a95e93a0bb0d6fa83402c8
Leonardo Taype Fernandez <93153931+Leo2426@users.noreply.github.com>
2023-10-27 19:14:34 -0500

    chore: added files

---
+++ file.py
@@ -0,0 +1,4 @@
+arr = [108, 105, 110, 101]
+
+for element in arr:
+    print(chr(element), end="")
--- main.py
+++ main.py
@@ -1,23 +1,5 @@
-nota = "   Hola    "
+import urllib.request, urllib.parse, urllib.error
 
-# # Imprimir desde la tercera letra hasta la ultima primera letra
-# print(nota[2:])
-#
-# # Imprimir desde la primera letra hasta la tercera letra
-# print(nota[:3])
-#
-# # Imprimir desde la primera letra hasta la ultima letra
-# print(nota[:])
-#
-# # Imprimir desde la primera letra hasta la ultima letra saltando de 2 en 2
-# print(nota[::2])
-#
-
-# for n in nota:
-#     print(n)
-
-text = "X-DSPAM-Confidence:    0.8475"
-start_position = text.find(':') + 1    # Encuentra la posición del ':' y suma 1 para omitir el espacio
-number_str = text[start_position:]     # Obtiene la subcadena a partir de la posición encontrada
-# number = float(number_str)             # Convierte la subcadena en un número de punto flotante
-print(number_str)
+fhand = urllib.request.urlopen('http://www.dr-chuck.com/page1.htm')
+for line in fhand:
+    print(line.decode().strip())
